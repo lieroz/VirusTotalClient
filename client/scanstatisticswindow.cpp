07686b6e9b459f71195c314ba26ba60557f235de
lieroz <a.kamakin@icloud.com>
2017-01-30 18:56:31 +0200

    Added simple scan statistics.

---
+++ client/scanstatisticswindow.cpp
@@ -0,0 +1,6 @@
+#include "scanstatisticswindow.h"
+
+ScanStatisticswindow::ScanStatisticswindow()
+{
+
+}
---
+++ client/scanstatisticswindow.h
@@ -0,0 +1,12 @@
+#ifndef SCANSTATISTICSWINDOW_H
+#define SCANSTATISTICSWINDOW_H
+
+#include <QtCore/qglobal.h>
+
+class ScanStatisticswindow
+{
+	public:
+		ScanStatisticswindow();
+};
+
+#endif // SCANSTATISTICSWINDOW_H
